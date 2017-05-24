c464731721d3a38d68019b0489d903fc1bf4114f
dwleonhardt <dwleonhardt@gmail.com>
2017-05-24 14:19:46 -0600

    bcrypt done

--- solution.js
+++ solution.js
@@ -1,9 +1,11 @@
+const bcrypt = require('bcrypt');
+const rounds = 8;
 function hash(plaintext) {
-
+  return bcrypt.hashSync(plaintext, rounds);
 }
 
 function compare(plaintext, hash) {
-
+  return bcrypt.compareSync(plaintext, hash);
 }
 
 module.exports = {
